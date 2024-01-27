fe1f889a52052170f5aa741d01fb37f7ee5cefea
Chamath <chamath@flexiata.lk>
2024-01-27 15:12:51 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: primary,
         ),
-        initialRoute: '/home',
+        initialRoute: '/initsplash',
         routes: AppRoutes.getRoutes(),
       ),
     );
