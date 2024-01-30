3c3df4952b67c13535d32b31e4f4629486a9c68a
Chamath <chamath@flexiata.lk>
2024-01-30 10:26:48 +0530

    d

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:koreanlms/constants/primary_colors.dart';
 import 'package:koreanlms/providers/authentication/signup_provider.dart';
@@ -6,8 +7,9 @@ import 'package:koreanlms/providers/mobile_validation/phone_validation_provider.
 import 'package:koreanlms/routes/app_routes.dart';
 import 'package:provider/provider.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
 }
 
 class MyApp extends StatelessWidget {
