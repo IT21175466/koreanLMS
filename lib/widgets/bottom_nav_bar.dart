12cac9b90ff020abbf074303900a805bdc844958
Chamath <chamath@flexiata.lk>
2024-05-07 07:41:22 +0530

    Update bottom_nav_bar.dart

--- lib/widgets/bottom_nav_bar.dart
+++ lib/widgets/bottom_nav_bar.dart
@@ -12,7 +12,7 @@ class CustomBottomNavigationBar extends StatelessWidget {
       builder: (BuildContext context,
           BottomNavBarProvider bottomNavigationProvider, Widget? child) {
         return Container(
-          height: Platform.isIOS ? 92 : 70,
+          height: Platform.isIOS ? 92 : 60,
           decoration: BoxDecoration(
             color: Colors.white,
             border: Border.symmetric(
@@ -31,24 +31,28 @@ class CustomBottomNavigationBar extends StatelessWidget {
               BottomNavigationBarItem(
                 icon: Icon(
                   Icons.home,
+                  size: 20,
                 ),
                 label: "Home",
               ),
               BottomNavigationBarItem(
                 icon: Icon(
                   Icons.question_answer,
+                  size: 20,
                 ),
                 label: "Quizes",
               ),
               BottomNavigationBarItem(
                 icon: Icon(
                   Icons.notifications,
+                  size: 20,
                 ),
                 label: "Notifications",
               ),
               BottomNavigationBarItem(
                 icon: Icon(
                   Icons.settings,
+                  size: 20,
                 ),
                 label: "Settings",
               ),
@@ -59,9 +63,11 @@ class CustomBottomNavigationBar extends StatelessWidget {
             selectedLabelStyle: TextStyle(
               fontWeight: FontWeight.w700,
               fontFamily: 'Poppins',
+              fontSize: 12,
             ),
             unselectedLabelStyle: TextStyle(
               fontFamily: 'Poppins',
+              fontSize: 12,
             ),
             selectedItemColor: AppColors.orangeColor,
             unselectedItemColor: AppColors.accentColor,
